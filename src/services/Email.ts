61cceb85ec71a5e7557eb5f090db9c08379906e3
MohamedMahmoud01110 <mohamed.mahmoud14532@gmail.com>
2026-07-16 05:50:54 +0300

    update otp

--- src/services/Email.ts
+++ src/services/Email.ts
@@ -1,22 +1,29 @@
 import nodemailer from 'nodemailer';
 
 export const sendEmailOtp = async (email: string, otp: string) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: 'Dentora Verification Code',
-    html: `
-      <h2>Your verification code is:</h2>
-      <h1>${otp}</h1>
-      <p>This code expires in 5 minutes.</p>
-    `,
-  });
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'Dentora Verification Code',
+      html: `
+        <h2>Your verification code is:</h2>
+        <h1>${otp}</h1>
+        <p>This code expires in 5 minutes.</p>
+      `,
+    });
+
+    console.log(info);
+  } catch (err) {
+    console.error('Email Error:', err);
+    throw err;
+  }
 };
